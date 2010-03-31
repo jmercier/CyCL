a6d448c3a0a0785bef5e770377df8a4f9b8bbb7d
J-Pascal Mercier <jean-pascal.mercier@inrialpes.fr>
2010-03-31 22:42:09 +0200

    added config

---
+++ config.py
@@ -0,0 +1,4 @@
+opencl_include_dir = '/usr/local/stream/include'
+#opencl_include_dir = '/usr/local/cuda/include'
+opencl_library_dir = '/usr/local/stream/lib'
+#opencl_library_dir = '/usr/local/cuda/lib'
