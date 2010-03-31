70e7e9f70ac58015893a6fb7609e0c302589e1fe
J-Pascal Mercier <jean-pascal.mercier@inrialpes.fr>
2010-03-31 22:41:51 +0200

    Move things and other

--- configs.py
+++
@@ -1,2 +0,0 @@
-opencl_include_dir = '/usr/local/stream/include' # '/usr/local/cuda/include'
-opencl_library_dir = '/usr/local/stream/lib' # '/usr/local/cuda/lib'
--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
+import os
+
+from config import *
 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-from configs import *
+from mako.template import Template
+print "building templates"
+for file in os.listdir("templates"):
+    with open(os.path.join("templates", file)) as input:
+        t = Template(input.read()).render()
+    with open(os.path.join("src/%s" % (file[:-5])), 'w') as output:
+        output.write(t)
+
 
 opencl_srcs = ['src/opencl.pyx']
-opencl_extension = Extension("cycl/opencl", opencl_srcs,
+opencl_extension = Extension("cycl.opencl", opencl_srcs,
                              libraries = ["OpenCL"],
                              library_dirs = [opencl_library_dir],
-                             include_dirs = [opencl_include_dir])
+                             include_dirs = [opencl_include_dir, 'src'])
+
+cycl_srcs = ['cycl']
 
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [opencl_extension],
+    packages = ['cycl', 'cycl/defines'],
     name = 'CyCL',
     version = '0.0.1',
     description = "Cython Bindings for OpenCL",
